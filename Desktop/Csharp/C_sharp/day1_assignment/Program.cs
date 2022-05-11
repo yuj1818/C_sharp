f7228f658ab628e307cb2256be7c13c02306105d
yuj1818 <sonyujeong18@gmail.com>
2022-05-11 13:11:43 +0900

    day1

---
+++ Desktop/Csharp/C_sharp/day1_assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 2; i <= 9; i++)
+{
+    Console.WriteLine("{0}단", i);
+    Console.WriteLine("============================");
+    for (int j = 1; j <= 9; j++)
+    {
+        int result;
+        result = i * j;
+        Console.WriteLine("{0} * {1} = {2}", i, j, result);
+    }
+    Console.WriteLine("");
+}
---
+++ Desktop/Csharp/C_sharp/day1_ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("국어 : ");
+String korean = Console.ReadLine();
+Console.Write("영어 : ");
+String english = Console.ReadLine();
+Console.Write("수학 : ");
+String math = Console.ReadLine();
+
+int a = Convert.ToInt32(korean);
+int b = Convert.ToInt32(english);
+int c = Convert.ToInt32(math);
+int total = a + b + c;
+float average = total / 3;
+Console.WriteLine("총점 : {0}", total);
+Console.WriteLine("평균 : {0}", Math.Round(average,2));
+
